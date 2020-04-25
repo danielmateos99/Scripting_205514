5599ba233c45ab588fd134511b19ded96c57b039
danielmateos99 <63020032+danielmateos99@users.noreply.github.com>
2020-04-25 22:12:57 +0200

    Lab2_205514

---
+++ Introduction to Scripting Part 1/Assets/RW/Scripts/Dropsheep.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dropsheep : MonoBehaviour
+{
+    public float dropDestroyDelay; // 1
+    private Collider myCollider; // 2
+    private Rigidbody myRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCollider = GetComponent<Collider>();
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void Drop()
+    {
+        myRigidbody.isKinematic = false; // 1
+        myCollider.isTrigger = false; // 2
+        Destroy(gameObject, dropDestroyDelay); // 3
+    }
+
+}
